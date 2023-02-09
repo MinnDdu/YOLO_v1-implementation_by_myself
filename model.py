27cb4d745f1196955e77b96f6d96d73ba1e2ddb3
Minsoo <markkim1@naver.com>
2023-02-09 17:28:12 +0900

    UPDATE model.py - preprocessing layer added

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import os
 yolo_model = tf.keras.Sequential()
 
 max_num = len(tf.keras.applications.VGG16(weights='imagenet', include_top=False,  input_shape=(224, 224, 3)).layers)
-
+print(max_num)
 for i in range(0, max_num):
     yolo_model.add(tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3)).layers[i])
 
@@ -16,6 +16,10 @@ for layer in yolo_model.layers:
     if hasattr(layer, 'activation'):
         layer.activation = tf.keras.layers.LeakyReLU(alpha=0.1)
 # -----------------------------------------------------------------
+# data augmentation (직접 적당히 추가하였음)
+yolo_model.add(tf.keras.layers.RandomTranslation((-0.05, 0.05), (-0.05, 0.05))) # 높이 +- 5%, 너비 +- 5% => 총 20%까지 랜덤성 부여
+# tf 2.9, 2.10 버그로 인해 전처리 몇몇 레이어 사용시 학습속도가 느려진다는 현상이 발견된다고함... 해보고 안되면 지워보자
+
 # Head part
 yolo_model.add(tf.keras.layers.Conv2D(1024, (3,3), padding='same'))
 yolo_model.add(tf.keras.layers.LeakyReLU(0.1)) # conv의 activation func 역할
---
+++ preprocessing.py
@@ -0,0 +1,11 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+import shutil
+import xmltodict
+
+# 1. 이미지 전처리
+# 2. train/valid split
+# 3. 이미지 라벨링
+# 4. 데이터 증강
